e61cbe22b529629860cdd2469995062b91d236ce
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2016-04-29 18:50:16 -0400

    Changed response to async

--- scripts/get_num_players.js
+++ scripts/get_num_players.js
@@ -6,16 +6,9 @@ function get_num_players() {
 
   var url = "development_api_key.txt";
   var jsonFile = new XMLHttpRequest();
-  jsonFile.open("GET",url,true);
+  jsonFile.open("GET",url,false);
   jsonFile.send();
-
-  jsonFile.onreadystatechange =       function(){
-                                            if (jsonFile.readyState== 4 && jsonFile.status == 200){
-                                              console.log(jsonFile.responseText);
-                                              api_key = jsonFile.responseText;
-                                            }
-                                          }
-
+  api_key = jsonFile.responseText;
   console.log(api_key);
   var low = 70000000;
   var high = 80000000;
