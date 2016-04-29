62965200be09306eb450d2a0406b40146d98e0f2
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2016-04-29 19:18:47 -0400

    1st implementation of get_num_players

--- scripts/get_num_players.js
+++ scripts/get_num_players.js
@@ -1,15 +1,49 @@
-function get_num_players() {
+var server;
+var api_key;
+var xml_http;
 
-
-  var server = "na";
+function get_num_players(server_name) {
+  var server = server_name;
   var api_key;
 
-  var url = "development_api_key.txt";
-  var jsonFile = new XMLHttpRequest();
-  jsonFile.open("GET",url,false);
-  jsonFile.send();
-  api_key = jsonFile.responseText;
-  console.log(api_key);
+  var api_key_url = "development_api_key.txt";
+  xml_http = new XMLHttpRequest();
+  xml_http.open("GET", api_key_url, false);
+  xml_http.send();
+  api_key = xml_http.responseText;
+
   var low = 70000000;
   var high = 80000000;
+
+
+}
+
+function find_cutoff(low, high) {
+  var rate_control = true;
+  setTimeout(function() {rate_control = false;}, 1000);
+
+  if (high - low == 1){
+    console.log("= " + low);
+    return low;
+  }
+  
+  middle = ((low + high) / 2) >> 0;
+  var url = "https://" + server + ".api.pvp.net/api/lol/" + server + "/v1.4/summoner/" + middle + "?api_key=" + api_key;
+  console.log(url);
+
+  xml_http.open("GET", url, false);
+  xml_http.send();
+
+  if (xml_http.status == 200){
+    low = middle;
+    console.log("> " + middle);
+  }
+  else{
+    high = middle;
+    console.log("<" + middle);
+  }
+
+  while (rate_control){}
+
+  return find_cutoff(low, high);
 }
